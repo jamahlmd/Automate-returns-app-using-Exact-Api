1b4cc6bf898c9124b41d0302a8c0640ef5640db5
jamahlmd <jamah@dorivit.nl>
2018-02-02 17:55:51 +0100

    back to basic

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div id="element" class="content">
                 <div class="title m-b-md">
-                    Jams innit
+                    Retourivit
                 </div>
 
                 <div class="links">
