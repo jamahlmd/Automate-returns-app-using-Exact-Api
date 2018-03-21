28be6cadf631decd5ece810e95b8763824443438
jamahlmd <jamah@dorivit.nl>
2018-03-21 12:42:43 +0100

    Added Resellcontroller and API

---
+++ app/Http/Controllers/ResellController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Retour;
+use Illuminate\Http\Request;
+
+class ResellController extends Controller
+{
+
+
+    public function searchByName($name){
+
+        return $retourData = Retour::select('invoice_name','reason','created_at')->where('customer_name',$name)->get();
+    }
+}
